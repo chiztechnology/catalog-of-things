d6b19082486ac295058511341794239b5426e65c
Israel <chizunguisrael@gmail.com>
2023-04-10 22:37:44 +0200

    Call music functions in App.rb

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,16 +1,16 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize(publish_date, on_spotify)
-        super(publish_date)
-        @on_spotify = on_spotify
-    end
+  def initialize(publish_date, on_spotify)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 
-    private
+  private
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
